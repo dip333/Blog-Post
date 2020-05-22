582cd2500ae0bd77dabec4c443157db1b519f503
Dip Sidpara <sidip333@gmail.com>
2020-05-22 20:46:50 +0530

    first commit with app ready for deployment

---
+++ flaskblog/config.py
@@ -0,0 +1,14 @@
+import os
+
+
+class Config:
+    SECRET_KEY = '5791628bb0b13ce0c676dfe280ba245'
+                #9ff2a466f8acde731fa112424e380313
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'dippatel076@gmail.com'
+    MAIL_PASSWORD = '1234'
+
+
